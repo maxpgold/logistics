5a5be8b39d28a20f41f4e844b5db0e7b77538d3f
Max Gold <max@recombine.com>
2015-09-09 23:29:12 -0400

    clean up venues

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -7,23 +7,9 @@ class Venue < ActiveRecord::Base
   has_many :activities_venues
   has_many :scheduled_activities, through: :actvities_scheduled_periods
 
-  # reverse_geocoded_by :coordinates
-  # after_validation :reverse_geocode
-
-  GEO_FACTORY = RGeo::Geographic.spherical_factory(srid: 4326)
-
-  set_rgeo_factory_for_column :coordinates, GEO_FACTORY
-
-  geocoded_by :name do |record, results|
-    result = results.first
-
-    record.name = result.name # Store the name used for geocoding
-    record.coordinates = GEO_FACTORY.point(result.latitude, result.longitude)
-  end
-
-  def parse_coordinates
-    coords = "ST_GeographyFromText('#{coordinates}')"
-    lat,lng = coords.gsub(/[^-.0-9\s]/, "").strip.split(" ")
+  def distance_from(venue_2)
+    # returns distance in miles
+    parse_coordinates.present? && venue_2.parse_coordinates.present? ? Geocoder::Calculations.distance_between(parse_coordinates, venue_2.parse_coordinates) : 'NA'
   end
 
   def latitude
@@ -32,10 +18,10 @@ class Venue < ActiveRecord::Base
 
   def longitude
     parse_coordinates[1]
-  end
+  end 
 
-  def distance_from(venue_2)
-    # returns distance in miles
-    parse_coordinates.present? && venue_2.parse_coordinates.present? ? Geocoder::Calculations.distance_between(parse_coordinates, venue_2.parse_coordinates) : 'NA'
+  def parse_coordinates
+    coords = "ST_GeographyFromText('#{coordinates}')"
+    lat,lng = coords.gsub(/[^-.0-9\s]/, "").strip.split(" ")
   end
 end
